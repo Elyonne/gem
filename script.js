b06b22cbf831d0dc25fc4544a3292368efddea58
Elyonne <elodie.fels@gmail.com>
2019-09-05 14:21:27 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('#description').parallax("50%", 0.1);
+    $('#galerie').parallax("50%", 0.1);
+    $('#mot-doux').parallax("70%", 0.1);
+    $('#galerie-bg').parallax("70%", 0.5);
+    $('#mot-doux-bg').parallax("50%", 0.5);
+})
